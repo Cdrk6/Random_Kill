fbdf2e78908e8be16ba8974fbdd8c039696ad6d9
courault <courault.f@gmail.com>
2017-06-08 11:36:11 +0200

    Nettoyage

--- Documents/CPP_Projects/SDL1/build/Debug/GNU-Linux/GUI.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-Linux/GUI.o: GUI.cpp values.h Map.h
-
-values.h:
-
-Map.h:
--- Documents/CPP_Projects/SDL1/values.h
+++
@@ -1,8 +0,0 @@
-#ifndef VALUES_H
-#define VALUES_H
-
-#define WIDTH 500
-#define HEIGHT 500
-
-#endif /* VALUES_H */
-
